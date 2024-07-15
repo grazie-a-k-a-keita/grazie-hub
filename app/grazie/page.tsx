5b43e8225a0deb50e930b5017ed01c66e5506fc5
Ono Keita <oh.kitbsk.1028@icloud.com>
2024-07-15 10:22:38 +0000

    chore: Add vivus library to project dependencies

---
+++ app/grazie/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import SvgGrazie from '@/components/svg-animation/svg-grazie';
+import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className='flex flex-col'>
+      <div className='flex justify-center'>
+        <SvgGrazie />
+      </div>
+      <div
+        className={cn(
+          'flex justify-center transition-opacity duration-1000',
+          visible ? 'opacity-100' : 'opacity-0',
+        )}
+      >
+        <p className='max-w-screen-md text-center text-xl'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut molestiae quas minus
+          voluptates, modi quisquam odio provident unde dolor delectus, earum perferendis assumenda
+          tempora, iste expedita soluta consequatur inventore ad.
+        </p>
+      </div>
+    </div>
+  );
+}
